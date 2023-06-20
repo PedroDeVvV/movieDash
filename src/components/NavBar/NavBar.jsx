ab05cfbde8fedfedfb7b026295d5dd42314f14a7
PedroDeVvV <pedrohgs2004@gmail.com>
2023-06-20 13:39:57 -0300

    atualização

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,42 @@
-import {Link} from 'react-router-dom'
-import{BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
-import './NavBar.css'
+import "./NavBar.css";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!search) return
+
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  };
+
   return (
     <nav id="navbar">
-    <h2>
-      <Link to="/"><BiCameraMovie />MoviesDash</Link>
-    </h2>
-  <form >
-    <input type="text" placeholder="Busque um filme"/>
-    <button type="submit"><BiSearchAlt2/></button>
-  </form>
-  </nav>
-  )
-}
+      <h2>
+        <Link to="/">
+          <BiCameraMovie />
+          MoviesDash
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
